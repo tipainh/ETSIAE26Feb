9d722d6e06bf2bdd7dd856ba1a2a98298e237069
manuelpfer <manuelperez.aero@gmail.com>
2020-02-26 12:38:18 +0100

    prueba

---
+++ Ordenacion.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 12:23:05 2020
+
+@author: Manuuuuuu
+"""
+nums = 
+#def ordenacion(nums):
+    x= max(nums)
+    y= min(nums)
+    
+    lista=[x:y]
+    
+    
+    
+#    return 
