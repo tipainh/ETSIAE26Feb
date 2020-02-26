fbb5efde8b66b2585953c2e54b3d559a66c55891
tipainh <tiagoffcd97@gmail.com>
2020-02-26 12:37:09 +0100

    Prueba1

---
+++ Ordenacion.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 12:10:39 2020
+
+@author: TiagoFilipe
+"""
+
+n = 10
+
+import random
+lista1 = [random.randint(0,n) for i in range(n)]
+print(lista1)
