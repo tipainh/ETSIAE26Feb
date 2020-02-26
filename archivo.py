dce59c224d531f679601a24934dc0a1e61d26009
VictoriaRuizR <victoriaruizramos1998@gmail.com>
2020-02-26 12:47:33 +0100

    prueba

---
+++ archivo.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+def ordenar(x):
+    n = len(x)
+    i = 0
+    for i:
+        i = i + 1
+        mediavalores = x[i] 
+        
+    
+    if x[0] < mediavalores:
+        x[0] = x[0]
+    else:
+        x[(n/2.0)+1] = x[0]
+    
+    return listaordenada
