71ee3f6cf16efb0df67287dc8878fbfc533b0967
MariaAlonso24 <m.alonsoalvarez24@gmail.com>
2020-02-26 12:39:42 +0100

    Prueba1

---
+++ temp.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+def ordenacion(v1):
+    return v2
+
+for i=0,n/2:
+    a(i)=v1(i)
+for i=n/2,n:
+    v1(i)=b(i)
+    
+if a(i)>b(i):
+    c(i)=[b(i),a(i)]
+    else:
+        c(i)=[a(i),b(i)]
+
+
+
+if__name__=='__main__':
+    
